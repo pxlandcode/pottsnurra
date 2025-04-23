8acc9d29adeeea1f5fe94863f398fc7c76dab48a
PierreElmen <pierre.elmen@gmail.com>
2025-04-23 13:45:19 +0200

    .

--- src/lib/components/widgets/GrossSalary.svelte
+++ src/lib/components/widgets/GrossSalary.svelte
@@ -8,7 +8,7 @@
 <Card header="Möjlig bruttolön">
 	<div class="flex flex-col items-center gap-2">
 		<span class="text-4xl font-medium text-primary mb-4">
-			{formatterToSek.format(salary)}/må
+			{formatterToSek.format(salary)}/mån
 		</span>
 	</div>
 </Card>
